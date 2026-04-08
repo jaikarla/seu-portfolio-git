9ff1d5ef4dc92f7d847b9a4f8226611abbd9d564
jaikarla <jaiannykarla12@gmail.com>
2026-04-07 22:37:03 -0300

    docs(hello): adiciona comentários para melhor entendimento

--- exercicios/ex01-commits/hello.cpp
+++ exercicios/ex01-commits/hello.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <string>
+#include <string> // Inclui a biblioteca para manipulação de strings
 
 int main() {
     std::string nome;
-    std::cout << "Digite seu nome: ";
+    std::cout << "Digite seu nome: "; // Solicita ao usuário que digite seu nome
     std::getline(std::cin, nome);
-    std::cout << "Hello, " << nome << "!" << std::endl;
+    std::cout << "Hello, " << nome << "!" << std::endl; // Exibe uma mensagem de saudação personalizada usando o nome fornecido pelo usuário
     return 0;
 }
