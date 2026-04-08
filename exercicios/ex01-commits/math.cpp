90949ced741cc8e2d4ec3fa2970729cede0d2b90
jaikarla <jaiannykarla12@gmail.com>
2026-04-07 22:41:05 -0300

    refactor(ex01): melhoria a organização do código

--- exercicios/ex01-commits/math.cpp
+++ exercicios/ex01-commits/math.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-using namespace std;
+
+// Programa simples de calculadora que realiza operações básicas com dois números inteiros
 
 int main() {
     int num1, num2;
-    
-    cout << "Digite o primeiro número: ";
-    cin >> num1;
-    
-    cout << "Digite o segundo número: ";
-    cin >> num2;
-    
-    cout << "\nSoma: " << (num1 + num2) << endl;
-    cout << "Subtração: " << (num1 - num2) << endl;
-    cout << "Multiplicação: " << (num1 * num2) << endl;
-    cout << "Divisão: " << (num1 / num2) << endl;
-    
+
+    std::cout << "Digite o primeiro número: ";
+    std::cin >> num1;
+
+    std::cout << "Digite o segundo número: ";
+    std::cin >> num2;
+
+    std::cout << "\nSoma: " << (num1 + num2) << std::endl;
+    std::cout << "Subtração: " << (num1 - num2) << std::endl;
+    std::cout << "Multiplicação: " << (num1 * num2) << std::endl;
+    std::cout << "Divisão: " << (num1 / num2) << std::endl;
+
     return 0;
 }
