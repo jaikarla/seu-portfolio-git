fb7fe9474ff4cc686c9998346d55e7eefe464bc4
jaikarla <jaiannykarla12@gmail.com>
2026-04-07 22:33:32 -0300

    feat(hello-world): adiciona entrada do nome do usuário

--- exercicios/ex01-commits/hello.cpp
+++ exercicios/ex01-commits/hello.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string nome;
+    std::cout << "Digite seu nome: ";
+    std::getline(std::cin, nome);
+    std::cout << "Hello, " << nome << "!" << std::endl;
     return 0;
 }
